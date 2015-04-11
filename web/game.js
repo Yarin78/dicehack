2593e173ea84a1ac3dc1e1ff55706dc454a848c8
Jimmy Mårdell <yarin@spotify.com>
2015-04-11 18:04:10 +0200

    wip

--- web/game.js
+++ web/game.js
@@ -13,13 +13,16 @@ bgImage.onload = function () {
 };
 bgImage.src = "images/outer-space.jpg";
 
-var pseudo_height = function(x) {
-	x = Math.round(x / 50)
+BLOCK_SIZE = 10
+MAX_TERRAIN_HEIGHT = 150
+MIN_TERRAIN_HEIGHT = 40
 
-}
+height = {}
 
-var ground_level = function(x) {
-
+var get_height = function(x) {
+	if (height[x]) return height[x];
+	height[x] = canvas.height - (Math.random() * (MAX_TERRAIN_HEIGHT - MIN_TERRAIN_HEIGHT) + MIN_TERRAIN_HEIGHT);
+	return height[x]
 }
 
 // Draw everything
@@ -29,25 +32,33 @@ var render = function () {
 	}
 
 	ctx.beginPath();
+	adj = Math.floor(scrollx / BLOCK_SIZE) * BLOCK_SIZE
+	ctx.moveTo(-scrollx, canvas.height)
 
-	ctx.moveTo(200 + scrollx, 30);
-	ctx.lineTo(300 + scrollx, 100);
-	ctx.strokeStyle="red";
-	ctx.lineWidth=5;
+	for (x = adj; x <= canvas.width + adj + BLOCK_SIZE; x += BLOCK_SIZE) {
+		ctx.lineTo(x - scrollx, get_height(x))
+
+//		ctx.quadraticCurveTo(x - scrollx - BLOCK_SIZE / 2, (get_height(x) + get_height(x - BLOCK_SIZE)) * 0.8, x - scrollx, get_height(x))
+	}
+
+	ctx.lineTo(canvas.width + adj + BLOCK_SIZE - scrollx, canvas.height);
+	ctx.closePath();
+
+	var gradient=ctx.createLinearGradient(0,canvas.height,0,canvas.height-MAX_TERRAIN_HEIGHT);
+    gradient.addColorStop("0","red");
+    gradient.addColorStop("0.4","green");
+    gradient.addColorStop("1.0","lightgreen");
+    ctx.fillStyle=gradient;
+	ctx.fill();
+
+	ctx.strokeStyle="green";
+	ctx.lineWidth=2;
 	ctx.stroke();
-
-
-	// Score
-	ctx.fillStyle = "rgb(250, 250, 250)";
-	ctx.font = "24px Helvetica";
-	ctx.textAlign = "left";
-	ctx.textBaseline = "top";
-	//ctx.fillText("Monsterrs caught: " + monstersCaught, 32, 32);
 };
 
 var update = function(modifier) {
 	scrollx += modifier;
-	console.log("x: " + scrollx);
+	//console.log("x: " + scrollx);
 }
 
 // The main game loop
